330ab8b2189468f42a720e28dc44be3ea6471793
олена городюк <135028180+OlenaUser1982@users.noreply.github.com>
2023-10-13 21:51:00 +0300

    task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,33 @@
+import Notiflix from 'notiflix';
+const form = document.querySelector('.form');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  const promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+  promise
+    .then(({ position, delay }) => {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+}
+function formSubmit(evt) {
+  evt.preventDefault();
+  let delayElem = evt.currentTarget.delay.valueAsNumber;
+  let stepElem = evt.currentTarget.step.valueAsNumber;
+  let amountElem = evt.currentTarget.amount.valueAsNumber;
+  for (let i = 1; i <= amountElem; i += 1) {
+    createPromise(i, delayElem);
+    delayElem += stepElem;
   }
 }
+form.addEventListener('submit', formSubmit);
