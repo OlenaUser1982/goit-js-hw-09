b4eccc10fdbcef034c4aa01b15bb0c6e0219287e
олена городюк <135028180+OlenaUser1982@users.noreply.github.com>
2023-10-13 19:37:20 +0300

    add

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+const start = document.querySelector('button[data-start]');
+const stop = document.querySelector('button[data-stop]');
+let timer = null;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+start.addEventListener('click', () => {
+  timer = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+    start.disabled = true;
+  }, 1000);
+});
 
+stop.addEventListener('click', () => {
+  clearInterval(timer);
+  start.disabled = false;
+});
